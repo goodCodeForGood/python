e30369aa0ae08b45a6f89cb7d0d876c1a8ae1eb4
goodCodeForGood <sahitihibane6@gmail.com>
2022-11-18 22:31:28 -0800

    Modify timed.py

--- timed.py
+++ timed.py
@@ -1,16 +1,9 @@
 import time
 
 def timeme(func):
-	def wrapper(inputVal):
-		timeOne = time.time()
-		func(inputVal)
-		timeTwo = time.time()
-		print("Total time", timeTwo - timeOne)
-	return wrapper
-	
-# time.sleep = timeme(time.sleep)
-
-# time.sleep(2)
-# time.sleep(4)
-# time.sleep(0)
-# time.sleep(100)
+    def wrapper():
+        timeOne = time.time()
+        func()
+        timeTwo = time.time()
+        print("Total time" , timeTwo - timeOne)
+    return wrapper
